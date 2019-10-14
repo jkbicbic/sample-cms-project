5860751a980f855e2651d3b1bff9b1e843dbffa2
John Kennedy Bicbic <jkbicbic@gmail.com>
2019-10-14 16:47:49 +0800

    added env for contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv')
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -72,6 +77,13 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `swwuur1qsnhl`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
