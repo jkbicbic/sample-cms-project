cfbe58732428a4d61385f0ddae65ce705aeb905a
John Kennedy Bicbic <jkbicbic@gmail.com>
2019-10-17 11:04:38 +0800

    added download local

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        // downloadLocal: true,
+        downloadLocal: true,
       },
     },
   ],
